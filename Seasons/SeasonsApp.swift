cd277ef461258256f210c50659afa9747f72aee9
Ethan Chen <echen@lcs.on.ca>
2024-11-20 10:09:39 -0500

    Initial Commit

---
+++ Seasons/SeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonsApp.swift
+//  Seasons
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct SeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
